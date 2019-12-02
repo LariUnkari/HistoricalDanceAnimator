23fc2515eb4acf19a7232a35c1969143d8ca539b
Lari Unkari <lariunkari@gmail.com>
2019-12-02 20:12:20 +0200

    First working version of animation and music synchronization, including a few quick sample dances but without the music (copyrights still apply).

---
+++ HistoricalDanceAnimator/Assets/Scripts/DancePreset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New DancePreset")]
+public class DancePreset : ScriptableObject
+{
+    public AudioClip songAudioClip;
+    public float songBPM = 100f;
+    public float silenceInBeginning;
+    public RuntimeAnimatorController animatorController;
+    public float animationBPS = 4f;
+}
---
+++ HistoricalDanceAnimator/Assets/Scripts/SynchronizeToMusic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SynchronizeToMusic : MonoBehaviour
+{
+    public DancePreset m_dancePreset;
+
+    public Animator m_animator;
+    public AudioSource m_audioSource;
+    
+	void Start()
+    {
+        if (m_animator != null)
+            m_animator.enabled = false;
+
+        if (m_audioSource != null && m_audioSource.playOnAwake)
+            m_audioSource.Stop();
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Play();
+    }
+
+    public void Play()
+    {
+        if (m_dancePreset == null)
+            return;
+
+        if (m_animator != null)
+        {
+            m_animator.runtimeAnimatorController = m_dancePreset.animatorController;
+            m_animator.speed = m_dancePreset.songBPM / (60f * (m_dancePreset.animationBPS > 0f ? m_dancePreset.animationBPS : 1f));
+            m_animator.enabled = true;
+        }
+
+        if (m_audioSource != null)
+        {
+            m_audioSource.clip = m_dancePreset.songAudioClip;
+
+            if (m_dancePreset.silenceInBeginning > 0f)
+                m_audioSource.time = m_dancePreset.silenceInBeginning;
+
+            m_audioSource.Play();
+        }
+    }
+}
